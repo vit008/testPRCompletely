3501fa4acbe4cb9e57b3e2028d5483eba0e7823c
Vitaliy Fedun <vfed@softsserveinc.com>
2018-12-24 20:26:28 +0200

    created ViewControllerVit_DevB

---
+++ testPRCompletely/testPRCompletely/ViewControllerVit_DevB.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerVit_DevB.swift
+//  testPRCompletely
+//
+//  Created by Vitaliy Fedun on 12/24/18.
+//  Copyright © 2018 Vitaliy Fedun. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerVit_DevB: ViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
